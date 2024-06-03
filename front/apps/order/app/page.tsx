f94759d6aec74b7cc47ce73a76a9b81597dd3910
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-06-03 23:25:29 +0900

    feat: 메인 페이지에 suspense 적용

--- front/apps/order/app/page.tsx
+++ front/apps/order/app/page.tsx
@@ -3,11 +3,13 @@
 import { Stack } from "@chakra-ui/react";
 import { Header } from "./components/header";
 import { Suspense } from "react";
+import MenuSkeleton from "./components/menu/skeleton";
 const Page: () => JSX.Element = () => {
   return (
     <>
       <Stack position="relative">
         <Header />
+        <Suspense fallback={<MenuSkeleton />}></Suspense>
       </Stack>
     </>
   );
