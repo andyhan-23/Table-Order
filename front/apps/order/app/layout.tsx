11e3e8cf6d2d60247dfa04f8de2306cd80be4c39
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-06-03 17:57:50 +0900

    feat: 레이아웃 구현

--- front/apps/order/app/layout.tsx
+++ front/apps/order/app/layout.tsx
@@ -1,1 +1,22 @@
 import { UIProvider } from "./components/providers";
+import { UIProvider as AppUIProvider } from "./components/providers";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  <html lang="ko">
+    <meta charSet="UTF-8" />
+    <meta
+      content="width=device-width, initial-scale=1.0 viewport-fit=cover"
+      name="viewport"
+    />
+    <meta
+      content="스마트폰으로 주문하는 테이블 오더 시스템입니다."
+      name="description"
+    />
+    <title>스마트 오더</title>
+    <body>
+      <UIProvider>
+        <AppUIProvider>{children}</AppUIProvider>
+      </UIProvider>
+    </body>
+  </html>;
+};
