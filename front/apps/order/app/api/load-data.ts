f302678c679f201f8ba51954f464a4b4d5d60d4d
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-06-04 23:11:45 +0900

    feat: 데이터를 가져오는 추상화 함수 구현

--- front/apps/order/app/api/load-data.ts
+++ front/apps/order/app/api/load-data.ts
@@ -1,1 +1,13 @@
 import apiClient from "./client";
+
+const loadData = async (path: string) => {
+  try {
+    const result = await apiClient.get(path);
+    return result.data;
+  } catch (error: unknown) {
+    console.error(`${path}에 해당하는 에러 메시지 출력: ${error}`);
+    return null;
+  }
+};
+
+export default loadData;
