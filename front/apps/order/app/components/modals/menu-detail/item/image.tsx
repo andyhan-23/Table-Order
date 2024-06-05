e2b5f57252591213e089720167bfa7e4b296bd1e
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-06-06 00:09:04 +0900

    feat: 디테일 메뉴를 표현한 모달 컴포넌트 구현

---
+++ front/apps/order/app/components/modals/menu-detail/item/image.tsx
@@ -0,0 +1,24 @@
+import { Box, Image as ChakraImage } from "@chakra-ui/react";
+
+type ImagePropsType = {
+  alt: string;
+  src: string;
+};
+
+const Image = ({ alt, src }: ImagePropsType) => {
+  return src ? (
+    <Box aspectRatio={1} h="100%" maxH="20vh" position="relative">
+      <ChakraImage
+        alt={alt}
+        borderRadius="12px"
+        fit="cover"
+        h="100%"
+        src={src}
+        w="100%"
+      />
+      {/* <NextImage alt={alt} borderRadius="0 12px 12px 0" fill sizes="(max-height: 600px) 25vw" src={src} /> */}
+    </Box>
+  ) : null;
+};
+
+export default Image;
