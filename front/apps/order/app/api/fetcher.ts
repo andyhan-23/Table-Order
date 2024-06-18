0ce407c661887207077f6742716073418f375626
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-06-18 11:23:45 +0900

    feat: axios interceptor를 사용하여 실제 서버가 아닌 mock server로 데이터 요청 보내는것으로 수정

--- front/apps/order/app/api/fetcher.ts
+++ front/apps/order/app/api/fetcher.ts
@@ -1,11 +1,47 @@
+// import axios from "axios";
+
+// let baseUrl = "";
+// try {
+//   baseUrl = `${window.location.protocol}//${window.location.host}`;
+// } catch {}
+
+// const fetcher = (url: string) =>
+//   axios.get(`${baseUrl}${url}`).then(({ data }) => data);
+
+// export default fetcher;
+
 import axios from "axios";
 
-let baseUrl = "";
-try {
-  baseUrl = `${window.location.protocol}//${window.location.host}`;
-} catch {}
+const axiosInstance = axios.create();
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    if (process.env.NODE_ENV === "development") {
+      if (typeof window === "undefined") {
+        const { server } = await import("../mock/server");
+        server.listen();
+      } else {
+        const { worker } = await import("../mock/browser");
+        worker.start();
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 const fetcher = (url: string) =>
-  axios.get(`${baseUrl}${url}`).then(({ data }) => data);
+  axiosInstance.get(url).then(({ data }) => data);
 
 export default fetcher;
