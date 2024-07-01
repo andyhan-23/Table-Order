7ca94e17ec2c0ea3a923207ed23536c14349ced1
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-07-01 17:44:57 +0900

    chore: 테스트

--- front/apps/order/app/components/menu/add-item-to-basket.tsx
+++ front/apps/order/app/components/menu/add-item-to-basket.tsx
@@ -10,6 +10,7 @@ const AddItemToBasketButton = ({
   onClick,
   quantity,
 }: AddItemToBasketButtonT): JSX.Element => {
+  console.log("test");
   const buttonClass = `bg-${quantity ? "skyblue" : "red"}`;
   const content =
     quantity > 0 ? (
